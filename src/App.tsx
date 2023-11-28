ca80bded4cbea9087e74ebaf92bc183128d336d8
Tony <tony.miasoedov@supponor.com>
2023-11-28 22:23:15 +0200

    no sus

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import {Route, Routes} from "react-router-dom";
 import {lazy, Suspense} from "react";
 import "./applications/styles/global.css";
 import MainMenu from "./widgets/main-menu/main-menu";
-
+import BlueTeam from "./pages/blue-team";
 
 
 const Main = lazy(() => import('./pages/main/main'));
 const About = lazy(() => import('./pages/about/about'));
 const Error = lazy(() => import('./pages/404/error'));
-const BlueTeam = lazy(() => import('./pages/blue-team'));
 const RedTeam = lazy(() => import('./pages/red-team'));
 
 
