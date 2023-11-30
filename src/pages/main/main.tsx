06cdeb400a580792b1cc5915d17d72769d526263
Tony <tony.miasoedov@supponor.com>
2023-11-30 18:24:45 +0200

    add storage

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,7 +1,26 @@
+import { getUrl } from 'aws-amplify/storage';
+import {useCallback, useEffect} from "react";
+
 export default function Main() {
+
+    const url = useCallback(async () => {
+        return await getUrl({
+            key: "sablinskie-peshery-5.jpg",
+            options: {
+                validateObjectExistence: true // defaults to false
+            }
+        });
+    }, []);
+
+
+    useEffect(() => {
+        url().then(console.log)
+    }, [])
+
     return (
         <div className="container">
             <h1>Main</h1>
+            <img src="https://rolebasedauths3shared182223-dev.s3.eu-north-1.amazonaws.com/blue/sablinskie-peshery-5.jpg" alt="" width="400px" height="100%"/>
         </div>
     )
 }
