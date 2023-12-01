1b2ed40763e3a14d56483950945ae4cd936108de
Tony <tony.miasoedov@supponor.com>
2023-12-01 08:57:45 +0200

    add img

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,26 +1,34 @@
-import { getUrl } from 'aws-amplify/storage';
-import {useCallback, useEffect} from "react";
+import {getUrl} from 'aws-amplify/storage';
+import {useCallback, useEffect, useState} from "react";
 
 export default function Main() {
 
+    const [image, setImage] = useState<string | null>(null);
+
     const url = useCallback(async () => {
-        return await getUrl({
-            key: "sablinskie-peshery-5.jpg",
-            options: {
-                validateObjectExistence: true // defaults to false
-            }
+        const img = await getUrl({
+            key: "blue/sablinskie-peshery-5.jpg",
         });
+        if (!img) {
+            console.error("Image not found");
+            return null;
+        }
+
+        setImage(img.url.href);
+
     }, []);
 
 
     useEffect(() => {
-        url().then(console.log)
+        url()
     }, [])
+
+    console.log("image", image)
 
     return (
         <div className="container">
             <h1>Main</h1>
-            <img src="https://rolebasedauths3shared182223-dev.s3.eu-north-1.amazonaws.com/blue/sablinskie-peshery-5.jpg" alt="" width="400px" height="100%"/>
+            <img src={image ? image : undefined} alt="" width="400px" height="100%"/>
         </div>
     )
 }
